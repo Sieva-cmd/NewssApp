387887dcad07e7a1128d248551374c64e2f0026f
sieva-cmd <musyokasieva@gmail.com>
2022-02-26 20:29:06 +0300

    create a source request

--- app/models.py
+++ app/models.py
@@ -1,10 +1,9 @@
 class News_source:
-    def __init__(self,id,name,description,category,country):
-        self.id =id
+    def __init__(self,name,description,url):
+    
         self.name =name
         self.description =description
-        self.category =category
-        self.country =country
+        self.url =url
         
 
 
