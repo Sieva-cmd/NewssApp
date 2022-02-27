1768962ec73eeef4e761d7033a82c29d56a38097
sieva-cmd <musyokasieva@gmail.com>
2022-02-27 20:38:39 +0300

    add test file for testing news sources class

--- tests/sources_test.py
+++ tests/sources_test.py
@@ -1,20 +1,20 @@
 
-# import unittest
+import unittest
 
-# from app.models import News_source
+from app.models import News_source
 
-# class SourcesTest(unittest.TestCase):
-#     """
-#     Test Class to test the behaviour of the Article class
-#     """
-#     def Setup(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_source = News_source('ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com')
+class SourcesTest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the Article class
+    """
+    def Setup(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = News_source('ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.','https://abcnews.go.com')
         
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_source,News_source))    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,News_source))    
     
     
 #     def test_init(self):
